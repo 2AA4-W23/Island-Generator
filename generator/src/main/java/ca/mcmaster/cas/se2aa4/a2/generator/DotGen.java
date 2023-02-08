37842eb43d7517235f7c3dc4f207d85f41a64a3e
Omar Al-Asfar <alasfaro@mcmaster.ca>
2023-02-08 17:11:45 -0500

    decimal precision

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
@@ -21,7 +21,11 @@ public class DotGen {
         // Create all the vertices
         for (int x = 0; x <= width; x += square_size) {
             for (int y = 0; y <= height; y += square_size) {
-                vertices.add(Vertex.newBuilder().setX((double) x).setY((double) y).build());
+                double xVal = (double) x / 100;
+                double yVal = (double) y / 100;
+                xVal = Math.round(xVal * 100.0) / 100.0;
+                yVal = Math.round(yVal * 100.0) / 100.0;
+                vertices.add(Vertex.newBuilder().setX(xVal).setY(yVal).build());
                 // vertices.add(Vertex.newBuilder().setX((double) x+square_size).setY((double)
                 // y).build());
                 // vertices.add(Vertex.newBuilder().setX((double) x).setY((double)
