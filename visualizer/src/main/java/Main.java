d97a21ece53bda2b67d6ead24ec40d4a4f51dad9
Virochaan <39714123+VirochaanRG@users.noreply.github.com>
2023-02-13 14:39:12 -0500

    Adding Debug Mode to visualize the connections between polygons

--- visualizer/src/main/java/Main.java
+++ visualizer/src/main/java/Main.java
@@ -13,6 +13,14 @@ public class Main {
         // Extracting command line parameters
         String input = args[0];
         String output = args[1];
+        Boolean debug = false;
+        try{
+            String debugText = args[2];
+            debug = true;
+        } catch (Exception e) {
+            System.out.println("No Third argument");
+        }
+
         // Getting width and height for the canvas
         Structs.Mesh aMesh = new MeshFactory().read(input);
         double max_x = Double.MIN_VALUE;
@@ -25,7 +33,7 @@ public class Main {
         Graphics2D canvas = SVGCanvas.build((int) Math.ceil(max_x), (int) Math.ceil(max_y));
         GraphicRenderer renderer = new GraphicRenderer();
         // Painting the mesh on the canvas
-        renderer.render(aMesh, canvas);
+        renderer.render(aMesh, canvas, debug);
         // Storing the result in an SVG file
         SVGCanvas.write(canvas, output);
         // Dump the mesh to stdout
--- visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
+++ visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
@@ -18,7 +18,7 @@ import java.util.List;
 public class GraphicRenderer {
 
     private static final int THICKNESS = 3;
-    public void render(Mesh aMesh, Graphics2D canvas) {
+    public void render(Mesh aMesh, Graphics2D canvas, Boolean debug) {
         canvas.setColor(Color.BLACK);
         Stroke stroke = new BasicStroke(0.5f);
         canvas.setStroke(stroke);
@@ -27,15 +27,42 @@ public class GraphicRenderer {
         List<Segment> SegmentList = aMesh.getSegmentsList();
         List<Polygon> PolygonList = aMesh.getPolygonsList();
 
+        int lowCentroidIdx = VertexList.size();
+
         for (Polygon p : PolygonList) {
             double x = VertexList.get(SegmentList.get(p.getSegmentIdxs(0)).getV1Idx()).getX();
             double y = VertexList.get(SegmentList.get(p.getSegmentIdxs(0)).getV1Idx()).getY();
+            lowCentroidIdx = Math.min(p.getCentroidIdx(), lowCentroidIdx);
             Color old = canvas.getColor();
             canvas.setColor(extractColor(p.getPropertiesList()));
+            if(debug){
+                canvas.setColor(new Color(0,0,0));
+            }
             Rectangle2D polygon = new Rectangle2D.Double(x,y,20,20);
             canvas.fill(polygon);
             canvas.setColor(old);
         }
+        int centroidIdx = 0;
+
+        if (debug) {
+            for (Polygon p : PolygonList) {
+                centroidIdx = p.getCentroidIdx();
+                int numNeighbours = p.getNeighborIdxsCount();
+                for (int i = 0; i < numNeighbours; i++) {
+                    double cx1 = VertexList.get(centroidIdx).getX();
+                    double cy1 = VertexList.get(centroidIdx).getY();
+                    double cx2 = VertexList.get(PolygonList.get(p.getNeighborIdxs(i)).getCentroidIdx()).getX();
+                    double cy2 = VertexList.get(PolygonList.get(p.getNeighborIdxs(i)).getCentroidIdx()).getY();
+                    Line2D line = new Line2D.Double(cx1, cy1, cx2, cy2);
+                    Color old1 = canvas.getColor();
+                    canvas.setColor(new Color(192, 192, 192));
+                    canvas.draw(line);
+                    canvas.setColor(old1);
+                }
+
+            }
+        }
+
         
         for (Segment s : SegmentList) {
             double x1 = VertexList.get(s.getV1Idx()).getX();
@@ -44,19 +71,30 @@ public class GraphicRenderer {
             double y2 = VertexList.get(s.getV2Idx()).getY();
             Color old1 = canvas.getColor();
             canvas.setColor(extractColor(s.getPropertiesList()));
+            if (debug){
+                canvas.setColor(new Color(90,90,90));
+            }
             Line2D line = new Line2D.Double(x1, y1, x2,y2);
             canvas.draw(line);
             canvas.setColor(old1);
         }
-
+        int count = 0;
         for (Vertex v: VertexList) {
             double centre_x = v.getX() - (THICKNESS/2.0d);
             double centre_y = v.getY() - (THICKNESS/2.0d);
             Color old = canvas.getColor();
             canvas.setColor(extractColor(v.getPropertiesList()));
+            if(debug){
+                if(count < lowCentroidIdx){
+                    canvas.setColor(new Color(90,90,90));
+                } else{
+                    canvas.setColor(new Color(255,0,0));
+                }
+            }
             Ellipse2D point = new Ellipse2D.Double(centre_x, centre_y, THICKNESS, THICKNESS);
             canvas.fill(point);
             canvas.setColor(old);
+            count ++;
         }
         
     }
