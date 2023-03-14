d416a776b03a5bf8e4035ba97e1967f8671c612c
Ibrahim Quraishi <quraishi@mcmaster.ca>
2023-03-14 14:11:06 -0400

    F33 - Created River generation file and Vertex Graph

---
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/island/RiverGen/AddRivers.java
@@ -0,0 +1,5 @@
+package ca.mcmaster.cas.se2aa4.island.RiverGen;
+
+public class AddRivers {
+    
+}
---
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/island/RiverGen/VertexGraph.java
@@ -0,0 +1,52 @@
+package ca.mcmaster.cas.se2aa4.island.RiverGen;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import ca.mcmaster.cas.se2aa4.a2.io.Structs;
+
+public class VertexGraph {
+
+    private int size;
+    private Set<Integer>[] adjacencyList;
+
+    public VertexGraph(List<Structs.Segment> segments, List<Structs.Vertex> vertices){
+        size = vertices.size();
+        adjacencyList = new HashSet[size];
+        for(Structs.Segment s : segments){
+            adjacencyList[s.getV1Idx()].add(s.getV2Idx());
+            adjacencyList[s.getV2Idx()].add(s.getV1Idx());
+        }
+    }
+
+    public boolean isConnected(Structs.Vertex v1, Structs.Vertex v2, List<Structs.Vertex> vertices){
+        try{
+            return adjacencyList[vertices.indexOf(v1)].contains(vertices.indexOf(v2));
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return false;
+        }
+    }
+
+    public boolean isConnected(int i1, int i2){
+        try{
+            return adjacencyList[i1].contains(i2);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return false;
+        }
+    }
+
+    public Set<Integer> getConnections(Structs.Vertex v, List<Structs.Vertex> vertices){
+        try {
+            return adjacencyList[vertices.indexOf(v)];
+        } catch (NumberFormatException e) {
+            return new HashSet<>();
+        }
+    }
+
+    public int size(){
+        return this.size;
+    }
+        
+}
