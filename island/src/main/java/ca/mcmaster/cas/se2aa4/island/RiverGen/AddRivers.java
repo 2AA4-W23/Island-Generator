d914e0755ae674c1c3dacefb65dfbcf3d355efe9
Omar Al-Asfar <alasfaro@mcmaster.ca>
2023-03-15 08:33:11 -0400

    F33 issues with river connectivity

--- island/src/main/java/ca/mcmaster/cas/se2aa4/island/RiverGen/AddRivers.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/island/RiverGen/AddRivers.java
@@ -22,11 +22,11 @@ public class AddRivers {
         List<Structs.Segment> newSegments = new ArrayList<>();
         newSegments.addAll(segments);
        //for(Structs.Vertex v : vertices) System.out.println(altEx.extractValues(v.getPropertiesList()));
-        for(int i = 0; i < numRivers; i++){
+        for(int i = 0; i < numRivers; i++) {
             int riverSize = 0;
             List<Structs.Segment> river = new ArrayList<>();
             do initialPoint = vertices.get(rng.nextInt(vertices.size()));
-            while(isWaterVertex(initialPoint, vertices, vpc, tiles));
+            while (isWaterVertex(initialPoint, vertices, vpc, tiles));
 
             Queue<Structs.Vertex> queue = new LinkedList<>();
             Set<Structs.Vertex> visitedVertices = new HashSet<>();
@@ -64,6 +64,16 @@ public class AddRivers {
                     }
                     currentVertex = parentVertex;
                 }
+                if (riverSize > 0) {
+                    Structs.Segment lastSegment = river.get(river.size() - 1);
+                    Structs.Vertex lastVertex = vertices.get(lastSegment.getV2Idx());
+                    Structs.Vertex nextVertex = getNextVertex(lastVertex, vertices, vvGraph);
+                    Structs.Segment nextSegment = vvGraph.getSegment(lastVertex, nextVertex, vertices);
+                    if (nextSegment != null) {
+                        river.add(nextSegment);
+                        riverSize++;
+                    }
+                }
             }
 
 //            Structs.Vertex nextVertex;
@@ -81,19 +91,20 @@ public class AddRivers {
 //            }
 //
 
-            if(riverSize == 0) {
+            if (riverSize == 0) {
                 i--;
                 continue;
             }
             System.out.println("ended river " + i + " with size " + riverSize);
-            for(Structs.Segment s : river) {
+
+            for (Structs.Segment s : river) {
                 Structs.Property color = Structs.Property.newBuilder().setKey("rgb_color").setValue("10,100,255").build();
                 Structs.Property segTag = Structs.Property.newBuilder().setKey("seg_tag").setValue("river").build();
                 Structs.Property thick = Structs.Property.newBuilder().setKey("thickness").setValue("5").build();
-                Structs.Property riverNum = Structs.Property.newBuilder().setKey("river_num").setValue(Integer.toString(i+1)).build();
+                Structs.Property riverNum = Structs.Property.newBuilder().setKey("river_num").setValue(Integer.toString(i + 1)).build();
                 Structs.Segment riverSeg = Structs.Segment.newBuilder(s).addProperties(color).addProperties(segTag).addProperties(riverNum).build();
                 int idx = segments.indexOf(s);
-                if(idx != -1) {
+                if (idx != -1) {
                     newSegments.set(idx, riverSeg);
                 }
             }
