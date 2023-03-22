ced96bb81ac060786744236f079fd7207c6a311b
Ibrahim Quraishi <quraishi@mcmaster.ca>
2023-03-21 20:31:21 -0400

    F38 - rivers can combine into thicker ones

--- island/src/main/java/ca/mcmaster/cas/se2aa4/island/RiverGen/AddRivers.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/island/RiverGen/AddRivers.java
@@ -4,7 +4,10 @@ import java.util.*;
 
 import ca.mcmaster.cas.se2aa4.a2.io.Structs;
 import ca.mcmaster.cas.se2aa4.island.Extractors.AltitudeExtractor;
+import ca.mcmaster.cas.se2aa4.island.Extractors.EdgeTagExtractor;
 import ca.mcmaster.cas.se2aa4.island.Extractors.Extractor;
+import ca.mcmaster.cas.se2aa4.island.Extractors.RiverExtractor;
+import ca.mcmaster.cas.se2aa4.island.Extractors.RiverThicknessExtractor;
 import ca.mcmaster.cas.se2aa4.island.Extractors.TileTagExtractor;
 import ca.mcmaster.cas.se2aa4.island.Graph.VertexGraph;
 import ca.mcmaster.cas.se2aa4.island.Graph.VertexPolygonConnections;
@@ -12,18 +15,20 @@ import ca.mcmaster.cas.se2aa4.island.Graph.VertexPolygonConnections;
 public class AddRivers {
 
     private static Extractor tileTagEx = new TileTagExtractor(); 
+    private static Extractor edgeTagEx = new EdgeTagExtractor(); 
+    private static Extractor riverEx = new RiverExtractor(); 
+    private static Extractor thickEx = new RiverThicknessExtractor(); 
     private static Extractor altEx = new AltitudeExtractor();
     private static Random rng = new Random();
 
     public static List<Structs.Segment> addRivers(List<Structs.Polygon> tiles, List<Structs.Segment> segments, List<Structs.Vertex> vertices, int numRivers){
-        VertexGraph vvGraph = new VertexGraph(segments, vertices);
-        VertexPolygonConnections vpc = new VertexPolygonConnections(tiles, segments, vertices);
-        Structs.Vertex initialPoint;
-       
        List<Structs.Segment> newSegments = new ArrayList<>();
        newSegments.addAll(segments);
         for(int i = 0; i < numRivers; i++) {
-            int riverSize = 0;
+            VertexGraph vvGraph = new VertexGraph(newSegments, vertices);
+            VertexPolygonConnections vpc = new VertexPolygonConnections(tiles, segments, vertices);
+            Structs.Vertex initialPoint;
+            int thickness = rng.nextInt(1,4);
             List<Structs.Segment> river = new ArrayList<>();
             do initialPoint = vertices.get(rng.nextInt(vertices.size()));
             while (isWaterVertex(initialPoint, vertices, vpc, tiles));
@@ -51,9 +56,9 @@ public class AddRivers {
                 queue.add(nextVertex);
             }
             if (riverVertex != null) {
+                Structs.Vertex currentVertex = riverVertex;
                 List<Structs.Segment> frwdSegs = new ArrayList<>();
                 List<Structs.Segment> bcwdSegs = new ArrayList<>();
-                Structs.Vertex currentVertex = riverVertex;
                 while (parent.containsKey(currentVertex)) {
                     Structs.Vertex parentVertex = parent.get(currentVertex);
                     Structs.Segment riverSegment = vvGraph.getSegment(currentVertex, parentVertex, vertices);
@@ -73,29 +78,45 @@ public class AddRivers {
                 }
                 river.addAll(frwdSegs);
                 river.addAll(bcwdSegs);
-                riverSize = river.size();
             }
-            if (riverSize == 0) {
+            if (river.size() == 0) {
                 i--;
                 continue;
-            }
-            river = addRiverProps(river, newSegments, i + 1);
-            System.out.println("ended river " + (i+1) + " with size " + riverSize);
+            }   
+            newSegments = addRiverProps(river, newSegments, i + 1, thickness);
+            //System.out.println("ended river " + (i+1) + " with size " + riverSize);
+            
+           // System.out.println(river.size());
         }
+        //for(Structs.Segment s : newSegments) System.out.print(!edgeTagEx.extractValues(s.getPropertiesList()).equals("river") ? "" : edgeTagEx.extractValues(s.getPropertiesList()) + "\n");     
         return newSegments;
     }
 
-    private static List<Structs.Segment> addRiverProps(List<Structs.Segment> river, List<Structs.Segment> allSegments, int riverNumber) {
+    private static List<Structs.Segment> addRiverProps(List<Structs.Segment> river, List<Structs.Segment> allSegments, int riverNumber, int thickness) {
         for (Structs.Segment s : river) {
-            System.out.println("seg added");
             Structs.Property color = Structs.Property.newBuilder().setKey("rgb_color").setValue("10,100,255").build();
             Structs.Property segTag = Structs.Property.newBuilder().setKey("seg_tag").setValue("river").build();
             Structs.Property riverNum = Structs.Property.newBuilder().setKey("river_num").setValue(riverNumber + "").build();
-            Structs.Segment riverSeg = Structs.Segment.newBuilder(s).addProperties(color).addProperties(segTag).addProperties(riverNum).build();
-            allSegments.set(allSegments.indexOf(s), riverSeg);
+            Structs.Property thick = Structs.Property.newBuilder().setKey("thickness").setValue(getThickness(s, allSegments, thickness) + "").build();
+            Structs.Segment riverSeg = Structs.Segment.newBuilder(s).addProperties(color).addProperties(segTag).addProperties(riverNum).addProperties(thick).build();
+            int index = allSegments.indexOf(s);
+            if(index != -1) allSegments.set(index, riverSeg); //prevents occasional OutOfBoundsErrors
+            else System.out.println("you fucked up");
         }
-        return river;
-    }    
+        return allSegments;
+    }  
+    
+    private static int getThickness(Structs.Segment segment, List<Structs.Segment> allSegments, int thick) {
+        String existingThickness = thickEx.extractValues(segment.getPropertiesList());
+        String tag = edgeTagEx.extractValues(segment.getPropertiesList());
+        if(tag.equals("river")){
+            try{
+                thick += Integer.parseInt(existingThickness);
+                System.out.println("merged!");
+            } catch (NumberFormatException e )  {}
+        }
+        return thick;
+    }  
 
     private static boolean isWaterVertex(Structs.Vertex v, List<Structs.Vertex> vertices, VertexPolygonConnections G, List<Structs.Polygon> tiles) {
         Set<Integer> connections = G.getConnections(v, vertices);
