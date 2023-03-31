e4e933ef9f8655e649f1dc9d4feb3ddfecbe4a05
Ibrahim Quraishi <quraishi@mcmaster.ca>
2023-03-31 16:20:06 -0400

    Created Node class

--- pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/pathfinder/Graph/Node.java
+++ pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/pathfinder/Graph/Node.java
@@ -1,5 +1,9 @@
 package ca.mcmaster.cas.se2aa4.pathfinder.Graph;
 
 public class Node extends Component{
-    
+    public final int id;
+
+    public Node(int id) {
+        this.id = id;
+    }
 }
