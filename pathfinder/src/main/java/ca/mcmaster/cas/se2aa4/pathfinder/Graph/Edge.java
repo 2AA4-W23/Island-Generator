255a2d06c961e421fc07d53bb0edc1aa5662b2b1
Ibrahim Quraishi <quraishi@mcmaster.ca>
2023-04-04 16:51:17 -0400

    Removed generic typing

--- pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/pathfinder/Graph/Edge.java
+++ pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/pathfinder/Graph/Edge.java
@@ -1,23 +1,23 @@
 package ca.mcmaster.cas.se2aa4.pathfinder.Graph;
 
-public class Edge<N extends Node> extends Component{
-    public final N v1;
-    public final N v2;
+public class Edge extends Component{
+    public final Node v1;
+    public final Node v2;
     public final boolean directed;
     public final int weight;
 
-    public Edge(N v1, N v2, int weight, boolean directed){
+    public Edge(Node v1, Node v2, int weight, boolean directed){
         this.v1 = v1;
         this.v2 = v2;
         this.weight = weight;
         this.directed = directed;
     }
 
-    public Edge(N v1, N v2){
+    public Edge(Node v1, Node v2){
         this(v1, v2, 1, false);
     }
 
-    public Edge(N v1, N v2, boolean directed){
+    public Edge(Node v1, Node v2, boolean directed){
         this(v1, v2, 1, directed);
     }
 
--- pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/pathfinder/Graph/Graph.java
+++ pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/pathfinder/Graph/Graph.java
@@ -1,5 +1,6 @@
 package ca.mcmaster.cas.se2aa4.pathfinder.Graph;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -8,37 +9,59 @@ import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
 
-public class Graph<N extends Node, E extends Edge<N>> implements Traversable<N> {
+public class Graph implements Traversable {
 
-    private Map<N, Set<N>> adjacencyList;
+    private Map<Node, Set<Node>> adjacencyList;
     private final int size;
-    
-    public Graph(List<N> nodes, List<E> edges){
+    private List<Node> nodes;
+
+    public Graph(List<Node> nodes, List<Edge> edges){
+        this.nodes = nodes;
         adjacencyList = new HashMap<>();
-        for(N node : nodes) adjacencyList.put(node, new HashSet<>());
-        for(E edge : edges){
-            N v1 = edge.v1, v2 = edge.v2;
+        for(Node node : nodes) adjacencyList.put(node, new HashSet<>());
+        for(Edge edge : edges){
+            Node v1 = edge.v1, v2 = edge.v2;
             addEdge(v1, v2);
             if(!edge.isDirected()) addEdge(v2, v1);
         }
         size = nodes.size();
     }
 
+    public Graph(int n){
+        nodes = new ArrayList<>();
+        adjacencyList = new HashMap<>();
+        size = n;
+        for(int i = 0; i < n; i++) nodes.add(new Node(i));
+        for(Node node : nodes) adjacencyList.put(node, new HashSet<>());
+    }
+
+    public void addDirectedEdge(int a, int b){
+        Node n1 = nodes.get(a), n2 = nodes.get(b);
+        addEdge(n1, n2);
+    }
+
+
+    public void addUndirectedEdge(int a, int b){
+        Node n1 = nodes.get(a), n2 = nodes.get(b);
+        addEdge(n1, n2);
+        addEdge(n2, n1);
+    }
+
     @Override
-    public List<N> shortestPath(N start, N end) {
-        Queue<LinkedList<N>> queue = new LinkedList<>();
-        Map<N, Integer> distance = new HashMap<>();
-        LinkedList<N> startPath = new LinkedList<>();
+    public List<Node> shortestPath(Node start, Node end) {
+        Queue<LinkedList<Node>> queue = new LinkedList<>();
+        Map<Node, Integer> distance = new HashMap<>();
+        LinkedList<Node> startPath = new LinkedList<>();
         startPath.add(start);
         distance.put(start, 0);
         queue.add(startPath);
         while(!queue.isEmpty()){
-            LinkedList<N> path = queue.remove();
-            N current = path.getLast();
+            LinkedList<Node> path = queue.remove();
+            Node current = path.getLast();
             if(current.id == end.id) return path;
-            for(N next : getNeighbors(current)){
+            for(Node next : getNeighbors(current)){
                 if(distance.containsKey(next)) continue;
-                LinkedList<N> nextPath = new LinkedList<>();
+                LinkedList<Node> nextPath = new LinkedList<>();
                 nextPath.addAll(path);
                 nextPath.add(next);
                 queue.add(nextPath);
@@ -49,16 +72,16 @@ public class Graph<N extends Node, E extends Edge<N>> implements Traversable<N> 
     }
 
     @Override
-    public boolean isNeighbor(N v1, N v2){
+    public boolean isNeighbor(Node v1, Node v2){
         return adjacencyList.get(v1).contains(v2);
     }
 
     @Override
-    public Set<N> getNeighbors(N a) {
+    public Set<Node> getNeighbors(Node a) {
         return adjacencyList.get(a);
     }
 
-    private void addEdge(N v1, N v2){
+    private void addEdge(Node v1, Node v2){
         adjacencyList.get(v1).add(v2);
     }
        
